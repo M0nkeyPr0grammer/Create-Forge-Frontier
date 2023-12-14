b24c762a977e7b99326ba9a8f7a8af36f8420e17
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2023-12-14 15:24:24 -0600

    New Molten Ores

--- kubejs/startup_scripts/Landscapes Reimagined/CustomFluids.js
+++ kubejs/startup_scripts/Landscapes Reimagined/CustomFluids.js
@@ -1,4 +1,6 @@
 StartupEvents.registry('fluid',event => {
+
+    // Creates Liquid Dragons Breath
     event.create('kubejs:liquid_dragons_breath')
         .color(0xc34781)
         .displayName('Liquid Dragons Breath')
@@ -7,4 +9,75 @@ StartupEvents.registry('fluid',event => {
         .viscosity(6000) // Similar to lava
         .temperature(1300) // Similar to lava
         .bucketItem; // Specify the bucket item here
+
+    // Creates Molten Tin    
+    event.create('kubejs:molten_tin')
+        .color(0xaaabaf)
+        .displayName('Molten Tin')
+        .bucketColor(0xaaabaf)
+        .thickTexture(0xaaabaf)
+        .viscosity(6000) // Similar to lava
+        .temperature(1300) // Similar to lava
+        .bucketItem; // Specify the bucket item here
+    
+    // Creates Molten Quartz
+    event.create('kubejs:molten_quartz')
+        .color(0x989186)
+        .displayName('Molten Quartz')
+        .bucketColor(0x989186)
+        .thickTexture(0x989186)
+        .viscosity(6000) // Similar to lava
+        .temperature(1300) // Similar to lava
+        .bucketItem; // Specify the bucket item here
+
+    // Creates Molten Ice Shard
+    event.create('kubejs:molten_ice_shard')
+        .color(0x8890b8)
+        .displayName('Molten Ice Shard')
+        .bucketColor(0x8890b8)
+        .thickTexture(0x8890b8)
+        .viscosity(6000) // Similar to lava
+        .temperature(1300) // Similar to lava
+        .bucketItem; // Specify the bucket item here
+
+    // Creates Molten Uranium
+    event.create('kubejs:molten_uranium')
+        .color(0x00fc00)
+        .displayName('Molten Uranium')
+        .bucketColor(0x00fc00)
+        .thickTexture(0x00fc00)
+        .viscosity(6000) // Similar to lava
+        .temperature(1300) // Similar to lava
+        .bucketItem; // Specify the bucket item here
+
+    // Creates Molten Bismuth
+    event.create('kubejs:molten_bismuth')
+        .color(0x555341)
+        .displayName('Molten Bismuth')
+        .bucketColor(0x555341)
+        .thickTexture(0x555341)
+        .viscosity(6000) // Similar to lava
+        .temperature(1300) // Similar to lava
+        .bucketItem; // Specify the bucket item here
+
+    // Creates Molten Irradium
+    event.create('kubejs:molten_irradium')
+        .color(0x21532b)
+        .displayName('Molten Irradium')
+        .bucketColor(0x21532b)
+        .thickTexture(0x21532b)
+        .viscosity(6000) // Similar to lava
+        .temperature(1300) // Similar to lava
+        .bucketItem; // Specify the bucket item here
+
+    // Create Molten Malachite
+    event.create('kubejs:molten_malachite')
+        .color(0x3c776d)
+        .displayName('Molten Malachite')
+        .bucketColor(0x3c776d)
+        .thickTexture(0x3c776d)
+        .viscosity(6000) // Similar to lava
+        .temperature(1300) // Similar to lava
+        .bucketItem; // Specify the bucket item here
+
 });
