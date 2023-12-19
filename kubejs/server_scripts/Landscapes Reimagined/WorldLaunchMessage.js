3bba033a155223fade2d3e7726be3cf47f34a1de
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2023-12-19 13:07:54 -0600

    Added custom login message to remind players to download resource packs

---
+++ kubejs/server_scripts/Landscapes Reimagined/WorldLaunchMessage.js
@@ -0,0 +1,14 @@
+
+
+// Listen for the player join event
+PlayerEvents.loggedIn((event) => {
+  const player = event.player;
+
+  // Send a welcome message in bold
+  player.tell('§lWelcome to Landscapes Reimagined Forge Frontier');
+
+  // Send the second message with clickable links
+  player.tell('To improve your overall player experience, download these two resource packs, hint click the links: \n');
+  player.tell('- Create Pipez Resource Pack: https://legacy.curseforge.com/minecraft/texture-packs/create-pipez/download/4691998 \n');
+  player.tell('- Create Style Construction Wands Resource Pack: https://legacy.curseforge.com/minecraft/texture-packs/create-style-construction-wands/download/4655257');
+});
