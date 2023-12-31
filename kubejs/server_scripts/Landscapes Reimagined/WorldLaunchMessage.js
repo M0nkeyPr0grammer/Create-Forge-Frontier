6b880dee3ba644c5b89b8c4888fdfd6690a7d45a
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2023-12-31 00:57:25 -0600

    Adds in Missing Saplings to Farming for Blockheads Market

--- kubejs/server_scripts/Landscapes Reimagined/WorldLaunchMessage.js
+++ kubejs/server_scripts/Landscapes Reimagined/WorldLaunchMessage.js
@@ -10,5 +10,5 @@ PlayerEvents.loggedIn((event) => {
   // Send the second message with clickable links
   player.tell('To improve your overall player experience, download these two resource packs, hint click the links: \n');
   player.tell('- Create Pipez Resource Pack: https://legacy.curseforge.com/minecraft/texture-packs/create-pipez/download/4691998 \n');
-  player.tell('- Create Style Construction Wands Resource Pack: https://legacy.curseforge.com/minecraft/texture-packs/create-style-construction-wands/download/4655257');
+  player.tell('- Create Style Construction Wands Resource Pack: https://legacy.curseforge.com/minecraft/texture-packs/create-style-construction-wands/download/4655257 \n');
 });
