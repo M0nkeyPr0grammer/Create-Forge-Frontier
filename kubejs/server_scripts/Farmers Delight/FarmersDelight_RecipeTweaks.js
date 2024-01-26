b5a7d407db6c0fee4ee26e77814414c4594a6bd8
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-01-25 23:11:28 -0600

    Added in Missing Recipe for Miners Cooking Guide

--- kubejs/server_scripts/Farmers Delight/FarmersDelight_RecipeTweaks.js
+++ kubejs/server_scripts/Farmers Delight/FarmersDelight_RecipeTweaks.js
@@ -17,6 +17,20 @@ ServerEvents.recipes(event => {
                 }
     )
 
+    event.shaped(
+        Item.of('create_central_kitchen:miners_cooking_guide',1),
+        [
+            'AB ',
+            'C  ',
+            '   '
+        ],
+        {
+            A: 'create:sturdy_sheet',
+            B: 'farmersdelight:canvas',
+            C: 'miners_delight:cave_carrot'
+        }
+    )
+
 
 
 })
