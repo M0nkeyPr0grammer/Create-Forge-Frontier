698d2e4b26bace2e97c1566758f2f04b2122ad44
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-05-05 11:29:05 -0500

    Readd BotanyPot Recipes, due to readdion of BotanyPots

---
+++ kubejs/server_scripts/BotanyPots/BotanyPot_RecipeRemovals.js
@@ -0,0 +1,763 @@
+ServerEvents.recipes(event => {
+
+// Standard Botany Pots
+
+    // Botany Pots - Remove recipes here
+	const compactHopperBotanyPots = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+    ];
+    
+    // Loop through the array and remove each item for Botany Pots
+    compactHopperBotanyPots.forEach(potType => {
+        event.remove({ id: `botanypots:botanypots/crafting/${potType}_compact_hopper_botany_pot` });
+    })
+
+    
+// Tiered Botany Pots
+
+    // Elite Pots
+    // Botany Tiers - Elite Hopper Pots - Remove recipes here
+    const compactHopperBotanyPotsElite = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+];            
+        
+    // Loop through the array and remove each item for Elite Hopper Pots
+    compactHopperBotanyPotsElite.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/elite_${potType}_compact_hopper_botany_pot_2` });
+    });
+
+    // Botany Tiers - Elite Compact Hopper Pots - Remove recipes here
+    const compactHopperBotanyPotsEliteCompact = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+];            
+        
+    // Loop through the array and remove each item for Elite Compact Hopper Pots
+    compactHopperBotanyPotsEliteCompact.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/elite_${potType}_compact_hopper_botany_pot` });
+    });    
+
+    // Botany Tiers - Elite Pots - Remove recipes here
+    const compactBotanyPotsEliteCompact = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+];            
+        
+    // Loop through the array and remove each item for Elite Pots
+    compactBotanyPotsEliteCompact.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/elite_${potType}_botany_pot` });
+    }); 
+
+
+    // Ultra Pots
+    // Botany Tiers - Ultra Hopper Pots - Remove recipes here
+    const compactHopperBotanyPotsUltra = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+];
+
+    // Loop through the array and remove each item for Ultra Hopper Pots
+    compactHopperBotanyPotsUltra.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/ultra_${potType}_compact_hopper_botany_pot_2` });
+    });
+
+    // Botany Tiers - Ultra Compact Hopper Pots - Remove recipes here
+    const compactHopperBotanyPotsUltraCompact = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+    
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+    
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+    
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+    
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+    
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+    
+        'terracotta',
+    
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+    
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+    
+    ];            
+            
+    // Loop through the array and remove each item for Ultra Compact Hopper Pots
+    compactHopperBotanyPotsUltraCompact.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/ultra_${potType}_compact_hopper_botany_pot` });
+    })
+
+    // Botany Tiers - Ultra Pots - Remove recipes here
+    const compactBotanyPotsUltraCompact = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+];            
+        
+    // Loop through the array and remove each item for Ultra Pots
+    compactBotanyPotsUltraCompact.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/ultra_${potType}_botany_pot` });
+    });
+
+    
+    // Creative Pots
+    // Botany Tiers - Creative Hopper Pots - Remove recipes here
+    const compactHopperBotanyPotsCreative = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+];
+
+    // Loop through the array and remove each item for Creative Hopper Pots
+    compactHopperBotanyPotsCreative.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/creative_${potType}_compact_hopper_botany_pot_2` });
+    });
+
+    // Botany Tiers - Creative Compact Hopper Pots - Remove recipes here
+    const compactHopperBotanyPotsCreativeCompact = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+    
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+    
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+    
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+    
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+    
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+    
+        'terracotta',
+    
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+    
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+    
+    ];            
+            
+    // Loop through the array and remove each item for Creative Compact Hopper Pots
+    compactHopperBotanyPotsCreativeCompact.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/creative_${potType}_compact_hopper_botany_pot` });
+    })
+
+    // Botany Tiers - Creative Pots - Remove recipes here
+    const compactBotanyPotsCreativeCompact = [
+        'black_concrete',
+        'black_glazed_terracotta',
+        'black_terracotta',
+        
+        'blue_concrete',
+        'blue_glazed_terracotta',
+        'blue_terracotta',
+
+        'brown_concrete',
+        'brown_glazed_terracotta',
+        'brown_terracotta',
+
+        'cyan_concrete',
+        'cyan_glazed_terracotta',
+        'cyan_terracotta',
+
+        'gray_concrete',
+        'gray_glazed_terracotta',
+        'gray_terracotta',
+
+        'green_concrete',
+        'green_glazed_terracotta',
+        'green_terracotta',
+
+        'light_blue_concrete',
+        'light_blue_glazed_terracotta',
+        'light_blue_terracotta',
+
+        'light_gray_concrete',
+        'light_gray_glazed_terracotta',
+        'light_gray_terracotta',
+
+        'lime_concrete',
+        'lime_glazed_terracotta',
+        'lime_terracotta',
+
+        'magenta_concrete',
+        'magenta_glazed_terracotta',
+        'magenta_terracotta',
+
+        'orange_concrete',
+        'orange_glazed_terracotta',
+        'orange_terracotta',
+
+        'pink_concrete',
+        'pink_glazed_terracotta',
+        'pink_terracotta',
+
+        'purple_concrete',
+        'purple_glazed_terracotta',
+        'purple_terracotta',
+
+        'red_concrete',
+        'red_glazed_terracotta',
+        'red_terracotta',
+
+        'terracotta',
+
+        'white_concrete',
+        'white_glazed_terracotta',
+        'white_terracotta',
+
+        'yellow_concrete',
+        'yellow_glazed_terracotta',
+        'yellow_terracotta',
+
+];            
+        
+    // Loop through the array and remove each item for Creative Pots
+    compactBotanyPotsCreativeCompact.forEach(potType => {
+        event.remove({ id: `botanypotstiers:crafting/creative_${potType}_botany_pot` });
+    });     
+
+})
