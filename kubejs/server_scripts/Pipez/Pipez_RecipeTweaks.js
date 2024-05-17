efa0b2e006a96a96a87b9fa875934c6249529ed9
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-05-17 12:39:01 -0500

    Updated Pipez Recipes

--- kubejs/server_scripts/Pipez/Pipez_RecipeTweaks.js
+++ kubejs/server_scripts/Pipez/Pipez_RecipeTweaks.js
@@ -3,49 +3,63 @@ ServerEvents.recipes(event => {
     event.remove({ id: 'pipez:item_pipe' }) // Removes Recipe
     event.remove({ id: 'pipez:fluid_pipe' }) // Removes Recipe
     event.remove({ id: 'pipez:energy_pipe' }) // Removes Recipe
+	event.remove({ id: 'pipez:universal_pipe'})
 
     // Creates New Item Pipe Recipe
 	event.shaped(
-		Item.of('pipez:item_pipe', 16),
+		Item.of('pipez:item_pipe', 2),
 		[
-			'AAA',
-			'BCB',
-			'AAA'
+			'ABA',
+			'A A',
+			'ABA'
 		],
 		{
-			A: 'create:andesite_alloy',
-			B: 'create:chute',
-			C: 'minecraft:redstone'
+			A: '#forge:rods/steel',
+			B: 'create:chute'
 		}
 	)
 
     // Creates New Fluid Pipe Recipe
     event.shaped(
-		Item.of('pipez:fluid_pipe', 16),
+		Item.of('pipez:fluid_pipe', 2),
 		[
-			'AAA',
-			'BCB',
-			'AAA'
+			'ABA',
+			'A A',
+			'ABA'
 		],
 		{
-			A: 'minecraft:copper_ingot',
-			B: 'create:fluid_pipe',
-			C: 'minecraft:redstone'
+			A: '#forge:rods/brass',
+			B: 'create:fluid_pipe'
 		}
 	)
 
     // Creates New Energry Pipe Recipe
     event.shaped(
-		Item.of('pipez:energy_pipe', 16),
+		Item.of('pipez:energy_pipe', 2),
 		[
-			'AAA',
-			'BCB',
-			'AAA'
+			'ABA',
+			'A A',
+			'ABA'
 		],
 		{
-			A: 'create:polished_rose_quartz',
-			B: 'create:electron_tube',
-			C: 'minecraft:redstone'
+			A: '#forge:rods/desh',
+			B: 'create:electron_tube'
+		}
+	)
+
+	// Creates Universal Pipe Recipe
+	event.shaped(
+		Item.of('pipez:universal_pipe', 6),
+		[
+			'ABC',
+			'D D',
+			'ABC'
+		],
+		{
+			A: 'pipez:item_pipe',
+			B: 'pipez:fluid_pipe',
+			C: 'pipez:energy_pipe',
+			D: 'create_new_age:overcharged_iron'
 		}
 	)
 
