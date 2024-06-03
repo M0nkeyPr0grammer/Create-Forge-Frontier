ec443f0dc622df6378623f9830bdbb72a0231237
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-03 13:59:51 -0500

    Molten Ore Rework Prep

--- kubejs/server_scripts/Create/MoltenOreReplacement_Recipes.js
+++ kubejs/server_scripts/Create/MoltenOreReplacement_Recipes.js
@@ -1,3 +1,136 @@
 ServerEvents.recipes(event => {
 
+    // Molten Copper
+        // Crushed Raw to Molten
+
+        // Block Raw to Molten
+
+        // Crushed Pile to Molten
+
+        // Raw to Molten
+
+        // Ore to Molten
+
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+        // Cut to Molten
+
+    // Molten Iron
+        // Crushed Raw to Molten
+
+        // Block Raw to Molten
+
+        // Crushed Pile to Molten
+
+        // Raw to Molten
+
+        // Ore to Molten
+
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+    // Molten Gold
+        // Crushed Raw to Molten
+
+        // Block Raw to Molten
+
+        // Crushed Pile to Molten
+
+        // Raw to Molten
+
+        // Ore to Molten
+
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+    // Molten Carbon
+        // Charcoal to Molten
+
+        // Ore to Molten
+ 
+        // Block to Molten
+
+        // Coal to Molten
+
+    // Molten Diamond
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+        // Ore to Molten
+
+        // Compacting
+
+    // Molten Zinc
+        // Ingot to Molten
+
+        // Crushed to Molten
+
+        // Raw to Molten
+
+        // Polished to Molten
+
+        // Crushed Pile to Molten
+
+        // Tiled to Molten
+
+        // Ore to Molten
+
+        // Raw Block to Molten
+
+        // Block to Molten
+
+    // Molten Brass
+
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+        // Zinc and Copper Mixing
+
+    // Molten Emerald
+
+        // Ore to Molten
+
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+        // Raw to Molten
+
+    // Molten Redstone
+
+        // Raw to Molten
+
+        // Ore to Molten
+
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+    // Molten Lapis
+
+        // Ore to Molten
+
+        // Ingot to Molten
+ 
+        // Block to Molten
+
+    // Molten Ancient Debris
+
+        // Raw to Molten
+
+        // Ingot to Molten
+
+    // Molten Netherite
+        // Debris and Gold Mixing
+
+        // Block to Molten
+
+        // Ingot to Molten
+
 })
