bf9ca92e6fc38a5b6823ea6d10f880f3cb661244
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-06 10:54:45 -0500

    Coal and Charcoal Molding Recipes

--- kubejs/server_scripts/Mold Recipes/CoalMixingMold_Recipes.js
+++ kubejs/server_scripts/Mold Recipes/CoalMixingMold_Recipes.js
@@ -1,3 +1,57 @@
 ServerEvents.recipes(event => {
 
+// Charcoal Mold Recipe
+event.custom({
+    type: 'create:mixing',
+    ingredients: [
+        {
+            fluid: 'forge_frontier:molten_carbon',
+            amount: 100
+        },
+        {
+            item: 'forge_frontier:coal_mold',
+            count: 1
+        },
+        {
+            tag: 'minecraft:logs',
+            count: 1
+        }
+    ],
+    results: [
+        {
+            item: 'minecraft:charcoal',
+            count: 1
+        },
+        {
+            item: 'forge_frontier:coal_mold'
+        }
+    ],
+    heatRequirement: 'superheated'
+}).id('forge_frontier:mixing/charcoal')
+
+// Coal Mold Recipe
+event.custom({
+    type: 'create:mixing',
+    ingredients: [
+        {
+            fluid: 'forge_frontier:molten_carbon',
+            amount: 100
+        },
+        {
+            item: 'forge_frontier:coal_mold',
+            count: 1
+        }
+    ],
+    results: [
+        {
+            item: 'minecraft:coal',
+            count: 1
+        },
+        {
+            item: 'forge_frontier:coal_mold'
+        }
+    ],
+    heatRequirement: 'superheated'
+}).id('forge_frontier:mixing/coal')
+
 })
