a7091f0e844e2cba18acec944c1ed785e664b32e
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-09 12:22:42 -0500

    New Block Tags for Molten Recipes compatibility

---
+++ kubejs/server_scripts/Forge Frontier/tags.js
@@ -0,0 +1,9 @@
+ServerEvents.tags('item', event => {
+event.add('forge:ores/ice_shard', 'ad_astra:moon_ice_shard_ore' )
+event.add('forge:ores/ice_shard', 'ad_astra:deepslate_ice_shard_ore' )
+event.add('forge:ores/ice_shard', 'ad_astra:mars_ice_shard_ore' )
+event.add('forge:ores/ice_shard', 'ad_astra:glacio_ice_shard_ore' )
+
+event.add('forge:ores/redstone', 'alexscaves:guanostone_redstone_ore')
+event.add('forge:ores/coal', 'alexscaves:coprolith_coal_ore')
+})
