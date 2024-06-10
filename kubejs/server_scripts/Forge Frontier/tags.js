0ed18508490948825c766fac55529fc5d7059b97
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-10 13:29:03 -0500

    Added in Ore tags for Ores

--- kubejs/server_scripts/Forge Frontier/tags.js
+++ kubejs/server_scripts/Forge Frontier/tags.js
@@ -1,9 +1,26 @@
 ServerEvents.tags('item', event => {
+
+// Ad Astra Ores
 event.add('forge:ores/ice_shard', 'ad_astra:moon_ice_shard_ore' )
 event.add('forge:ores/ice_shard', 'ad_astra:deepslate_ice_shard_ore' )
 event.add('forge:ores/ice_shard', 'ad_astra:mars_ice_shard_ore' )
 event.add('forge:ores/ice_shard', 'ad_astra:glacio_ice_shard_ore' )
+event.add('forge:ores/cheese','ad_astra:moon_cheese_ore')
+event.add('forge:ores/desh','ad_astra:moon_desh_ore')
+event.add('forge:ores/desh','ad_astra:deepslate_desh_ore')
+event.add('forge:ores/ostrum','ad_astra:mars_ostrum_ore')
+event.add('forge:ores/ostrum','ad_astra:deepslate_ostrum_ore')
+event.add('forge:ores/calorite','ad_astra:venus_calorite_ore')
+event.add('forge:ores/calorite','ad_astra:deepslate_calorite_ore')
 
+// Alex Caves Ores
 event.add('forge:ores/redstone', 'alexscaves:guanostone_redstone_ore')
 event.add('forge:ores/coal', 'alexscaves:coprolith_coal_ore')
+event.add('forge:ores/uranium', 'alexscaves:radrock_uranium_ore')
+
+// Enlightened End Ores
+event.add('forge:ores/bismuth','enlightened_end:bismuth_ore')
+event.add('forge:ores/irradium','enlightened_end:irradium_ore')
+event.add('forge:ores/malachite','enlightened_end:malachite_ore')
+
 })
