97f57e44c4c3e79b37e20044169c6ea471e51555
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-13 19:11:08 -0500

    Finished Refined Storage Rework

--- kubejs/server_scripts/RefinedStorage/RefinedStorage_RecipeTweaks.js
+++ kubejs/server_scripts/RefinedStorage/RefinedStorage_RecipeTweaks.js
@@ -13,8 +13,8 @@ ServerEvents.recipes(event => {
       event.shapeless(
         Item.of('refinedstorage:quartz_enriched_iron'),
         [
-          'create_dd:industrial_iron_ingot',
-          'create_dd:industrial_iron_ingot',
+          'create_new_age:overcharged_iron',
+          'create_new_age:overcharged_iron',
           'minecraft:quartz'
         ]
       ).id('forge_frontier:shapeless/quartz_enriched_iron'); 
@@ -51,6 +51,7 @@ ServerEvents.recipes(event => {
       
     // Mechanical Crafting Recipes
       // Creates New Grid Recipe using Tom's Simple Storage
+      event.remove({ id: 'refinedstorage:grid'})
       event.custom({
         type: "create:mechanical_crafting",
         pattern: [
@@ -63,7 +64,7 @@ ServerEvents.recipes(event => {
           B: Ingredient.of('refinedstorage:construction_core').toJson(),
           C: Ingredient.of('#forge:glass').toJson(),
           D: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
-          E: Ingredient.of('toms_storage:crafting_terminal').toJson(),
+          E: Ingredient.of('toms_storage:ts.crafting_terminal').toJson(),
           F: Ingredient.of('refinedstorage:destruction_core').toJson(),
         },
         result: Ingredient.of('refinedstorage:grid').toJson(),
@@ -71,30 +72,257 @@ ServerEvents.recipes(event => {
       }).id('forge_frontier:mechanical_crafting/grid')
 
       // Switches Portable Grid to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:portable_grid' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "EGE",
+          "ECE",
+          "EFE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          G: Ingredient.of('#refinedstorage:grid').toJson(),
+          F: Ingredient.of('#refinedstorage:fluid_grid').toJson(),
+          C: Ingredient.of('#refinedstorage:controller').toJson()
+        },
+        result: Ingredient.of('refinedstorage:portable_grid').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/portable_grid')
 
       // Switches Disk Drive Recipe to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:disk_drive' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "ECE",
+          "EME",
+          "EPE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          C: Ingredient.of('toms_storage:ts.storage_terminal').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          P: Ingredient.of('refinedstorage:advanced_processor').toJson()
+        },
+        result: Ingredient.of('refinedstorage:disk_drive').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/disk_drive')
 
       // Switches Controller Recipe to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:controller' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "EPE",
+          "SMS",
+          "ESE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          P: Ingredient.of('refinedstorage:advanced_processor').toJson(),
+          S: Ingredient.of('#forge:silicon').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson()
+        },
+        result: Ingredient.of('refinedstorage:controller').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/controller')
 
       // Switches Network Receiver to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:network_receiver' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "ANA",
+          "CMD",
+          "EEE"
+        ],
+        key: {
+          E: Ingredient.of('#forge:ender_pearls').toJson(),
+          C: Ingredient.of('refinedstorage:construction_core').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          D: Ingredient.of('refinedstorage:destruction_core').toJson(),
+          A: Ingredient.of('refinedstorage:advanced_processor').toJson(),
+          N: Ingredient.of('minecraft:netherite_ingot').toJson(),
+        },
+        result: Ingredient.of('refinedstorage:network_receiver').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/network_receiver')
 
       // Switches Network Transmitter to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:network_transmitter' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "EEE",
+          "CMD",
+          "ANA"
+        ],
+        key: {
+          E: Ingredient.of('#forge:ender_pearls').toJson(),
+          C: Ingredient.of('refinedstorage:construction_core').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          D: Ingredient.of('refinedstorage:destruction_core').toJson(),
+          A: Ingredient.of('refinedstorage:advanced_processor').toJson(),
+          N: Ingredient.of('minecraft:netherite_ingot').toJson(),
+        },
+        result: Ingredient.of('refinedstorage:network_transmitter').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/network_transmitter')
 
       // Switches Wireless Transmitter to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:wireless_transmitter' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "EPE",
+          "EME",
+          "EAE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          A: Ingredient.of('refinedstorage:advanced_processor').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          P: Ingredient.of('#forge:ender_pearls').toJson()
+        },
+        result: Ingredient.of('refinedstorage:wireless_transmitter').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/wireless_transmitter')
 
       // Switches Relay to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:relay' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "MCR"
+        ],
+        key: {
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          C: Ingredient.of('refinedstorage:cable').toJson(),
+          R: Ingredient.of('minecraft:redstone_torch').toJson(),
+        },
+        result: Ingredient.of('refinedstorage:relay').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/relay')
 
       // Switches Detector to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:detector' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "ERE",
+          "CMC",
+          "EPE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          C: Ingredient.of('minecraft:comparator').toJson(),
+          R: Ingredient.of('minecraft:redstone_torch').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          P: Ingredient.of('refinedstorage:improved_processor').toJson()
+        },
+        result: Ingredient.of('refinedstorage:detector').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/detector')
 
       // Switches Security Manager to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:security_manager' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "ECE",
+          "SMS",
+          "ESE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          C: Ingredient.of('#forge:chests').toJson(),
+          S: Ingredient.of('refinedstorage:security_card').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson()
+        },
+        result: Ingredient.of('refinedstorage:security_manager').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/security_manager')
 
       // Switches Disk Manipulator to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:disk_manipulator' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "ESE",
+          "CMD",
+          "ESE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          S: Ingredient.of('refinedstorage:storage_housing').toJson(),
+          C: Ingredient.of('refinedstorage:construction_core').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          D: Ingredient.of('refinedstorage:destruction_core').toJson()
+        },
+        result: Ingredient.of('refinedstorage:disk_manipulator').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/disk_manipulator')
 
       // Switches Crafter to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:crafter' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "ECE",
+          "AMA",
+          "EDE"
+        ],
+        key: {
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          C: Ingredient.of('refinedstorage:construction_core').toJson(),
+          A: Ingredient.of('refinedstorage:advanced_processor').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson(),
+          D: Ingredient.of('refinedstorage:destruction_core').toJson()
+        },
+        result: Ingredient.of('refinedstorage:crafter').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/crafter')
 
       // Switches Crafter Manager to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:crafter_manager' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "PCG",
+          "EMG",
+          "PCG"
+        ],
+        key: {
+          P: Ingredient.of('refinedstorage:advanced_processor').toJson(),
+          C: Ingredient.of('#refinedstorage:crafter').toJson(),
+          G: Ingredient.of('#forge:glass').toJson(),
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson()
+        },
+        result: Ingredient.of('refinedstorage:crafter_manager').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/crafter_manager')
 
       // Switches Crafting Monitor to Mechanical Crafting
+      event.remove({ id: 'refinedstorage:crafting_monitor' })
+      event.custom({
+        type: "create:mechanical_crafting",
+        pattern: [
+          "PAG",
+          "EMG",
+          "PAG"
+        ],
+        key: {
+          P: Ingredient.of('refinedstorage:advanced_processor').toJson(),
+          A: Ingredient.of('refinedstorage:pattern').toJson(),
+          G: Ingredient.of('#forge:glass').toJson(),
+          E: Ingredient.of('refinedstorage:quartz_enriched_iron').toJson(),
+          M: Ingredient.of('refinedstorage:machine_casing').toJson()
+        },
+        result: Ingredient.of('refinedstorage:crafting_monitor').toJson(),
+        acceptMirrored: false
+      }).id('forge_frontier:mechanical_crafting/crafting_monitor')
   
     // Sequenced Assembly Recipes
       // Creates Sequenced Assembly Recipe for Raw Basic Processor
