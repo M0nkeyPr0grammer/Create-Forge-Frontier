24a27a6aeb422245dbe821bb7ed5abacf8e8ea3b
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-21 13:25:39 -0500

    Moved to Recipe Proper File

--- kubejs/server_scripts/Crushing/OreCrushing.js
+++ kubejs/server_scripts/Crushing/OreCrushing.js
@@ -13,6 +13,19 @@ ServerEvents.recipes(event => {
     event.remove({ id: 'create_dd:crushing/scorchia_recycling'})
     event.remove({ id: 'create:crushing/scoria'})
 
+    // Creates Raw Redstone Crushing Recipe
+	event.custom({
+		type: "create:crushing",
+		ingredients: [
+			{ item: 'regions_unexplored:raw_redstone_block' }
+		],
+		processingTime: 400,
+		results: [
+			{ item: 'minecraft:redstone', count: 4 },
+			{ item: 'minecraft:redstone', chance: 0.10 }
+		]
+	}).id('forge_frontier:crushing/raw_redstone');
+
     // Amethyst Crushing Recipe
     event.custom({
 		type: "create:crushing",
