a987747cded73a8db4b65a7f4d0f5b9e0ffcde72
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-21 13:14:31 -0500

    Seething Recipes Code Cleanup

--- kubejs/server_scripts/Create/SeethingRecipes.js
+++ kubejs/server_scripts/Create/SeethingRecipes.js
@@ -1,231 +1,231 @@
 ServerEvents.recipes(event => {
+// Ore Seething Recipes
+    // Crushed Bismuth Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_bismuth_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'enlightened_end:bismuth_block'
+            },
+            {
+                chance: 0.60,
+                item: 'enlightened_end:bismuth_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_bismuth_pile')
 
-// Crushed Bismuth Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_bismuth_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'enlightened_end:bismuth_block'
-        },
-        {
-            chance: 0.60,
-            item: 'enlightened_end:bismuth_block'
-        }
-    ]
-})
+    // Crushed Calorite Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_calorite_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'ad_astra:calorite_block'
+            },
+            {
+                chance: 0.60,
+                item: 'ad_astra:calorite_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_calorite_pile')
 
-// Crushed Calorite Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_calorite_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'ad_astra:calorite_block'
-        },
-        {
-            chance: 0.60,
-            item: 'ad_astra:calorite_block'
-        }
-    ]
-})
+    // Crushed Desh Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_desh_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'ad_astra:desh_block'
+            },
+            {
+                chance: 0.60,
+                item: 'ad_astra:desh_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_desh_pile')
 
-// Crushed Desh Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_desh_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'ad_astra:desh_block'
-        },
-        {
-            chance: 0.60,
-            item: 'ad_astra:desh_block'
-        }
-    ]
-})
+    // Crushed Irradium Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_irradium_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'enlightened_end:irradium_block'
+            },
+            {
+                chance: 0.60,
+                item: 'enlightened_end:irradium_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_irradium_pile')
 
-// Crushed Irradium Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_irradium_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'enlightened_end:irradium_block'
-        },
-        {
-            chance: 0.60,
-            item: 'enlightened_end:irradium_block'
-        }
-    ]
-})
+    // Crushed Malachite Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_malachite_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'enlightened_end:malachite_block'
+            },
+            {
+                chance: 0.60,
+                item: 'enlightened_end:malachite_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_malachite_pile')
 
-// Crushed Malachite Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_malachite_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'enlightened_end:malachite_block'
-        },
-        {
-            chance: 0.60,
-            item: 'enlightened_end:malachite_block'
-        }
-    ]
-})
+    // Crushed Ostrum Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_ostrum_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'ad_astra:ostrum_block'
+            },
+            {
+                chance: 0.60,
+                item: 'ad_astra:ostrum_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_ostrum_pile')
 
-// Crushed Ostrum Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_ostrum_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'ad_astra:ostrum_block'
-        },
-        {
-            chance: 0.60,
-            item: 'ad_astra:ostrum_block'
-        }
-    ]
-})
+    // Crushed Tin Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_tin_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'create_dd:tin_block'
+            },
+            {
+                chance: 0.60,
+                item: 'create_dd:tin_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_tin_pile')
 
-// Crushed Tin Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_tin_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'create_dd:tin_block'
-        },
-        {
-            chance: 0.60,
-            item: 'create_dd:tin_block'
-        }
-    ]
-})
+    // Crushed Uranium Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'forge_frontier:crushed_uranium_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'alexscaves:block_of_uranium'
+            },
+            {
+                chance: 0.60,
+                item: 'alexscaves:block_of_uranium'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_uranium_pile')
 
-// Crushed Uranium Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'forge_frontier:crushed_uranium_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'alexscaves:block_of_uranium'
-        },
-        {
-            chance: 0.60,
-            item: 'alexscaves:block_of_uranium'
-        }
-    ]
-})
+    // Crushed Iron Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'create_compressed:crushed_iron_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'minecraft:iron_block'
+            },
+            {
+                chance: 0.60,
+                item: 'minecraft:iron_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_iron_pile')
 
-// Crushed Iron Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'create_compressed:crushed_iron_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'minecraft:iron_block'
-        },
-        {
-            chance: 0.60,
-            item: 'minecraft:iron_block'
-        }
-    ]
-})
+    // Crushed Gold Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'create_compressed:crushed_gold_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'minecraft:gold_block'
+            },
+            {
+                chance: 0.60,
+                item: 'minecraft:gold_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_gold_pile')
 
-// Crushed Gold Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'create_compressed:crushed_gold_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'minecraft:gold_block'
-        },
-        {
-            chance: 0.60,
-            item: 'minecraft:gold_block'
-        }
-    ]
-})
+    // Crushed Copper Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'create_compressed:crushed_copper_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'minecraft:copper_block'
+            },
+            {
+                chance: 0.60,
+                item: 'minecraft:copper_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_copper_pile')
 
-// Crushed Copper Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'create_compressed:crushed_copper_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'minecraft:copper_block'
-        },
-        {
-            chance: 0.60,
-            item: 'minecraft:copper_block'
-        }
-    ]
-})
-
-// Crushed Zinc Pile
-event.custom({
-    type: "create_dd:superheating",
-    ingredients: [
-        {
-            item: 'create_compressed:crushed_zinc_pile'
-        }
-    ],
-    results: [
-        {
-            item: 'create:zinc_block'
-        },
-        {
-            chance: 0.60,
-            item: 'create:zinc_block'
-        }
-    ]
-})
+    // Crushed Zinc Pile
+    event.custom({
+        type: "create_dd:superheating",
+        ingredients: [
+            {
+                item: 'create_compressed:crushed_zinc_pile'
+            }
+        ],
+        results: [
+            {
+                item: 'create:zinc_block'
+            },
+            {
+                chance: 0.60,
+                item: 'create:zinc_block'
+            }
+        ]
+    }).id('forge_frontier:seething/crushed_zinc_pile')
 
 })
