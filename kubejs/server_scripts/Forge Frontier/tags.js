3352fd7c1b429789af7b60cf9f90fd25e49f599a
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-24 14:42:56 -0500

    Added in Custom Tags, more may be added later

--- kubejs/server_scripts/Forge Frontier/tags.js
+++ kubejs/server_scripts/Forge Frontier/tags.js
@@ -23,4 +23,53 @@ event.add('forge:ores/bismuth','enlightened_end:bismuth_ore')
 event.add('forge:ores/irradium','enlightened_end:irradium_ore')
 event.add('forge:ores/malachite','enlightened_end:malachite_ore')
 
+// forge:templates/smithing
+event.add('forge:templates/smithing','minecraft:netherite_upgrade_smithing_template')
+event.add('forge:templates/smithing','forge_frontier:netherite_iron_upgrade')
+event.add('forge:templates/smithing','forge_frontier:netherite_gold_upgrade')
+event.add('forge:templates/smithing','forge_frontier:netherite_emerald_upgrade')
+event.add('forge:templates/smithing','forge_frontier:netherite_diamond_upgrade')
+event.add('forge:templates/smithing','cataclysm:ignitium_upgrade_smithing_template')
+event.add('forge:templates/smithing','enlightened_end:adamantite_smithing_template')
+event.add('forge:templates/smithing','ethuim:ethium_upgrade_smithing_template')
+event.add('forge:templates/smithing','endreborn:mysterious_upgrade_template')
+event.add('forge:templates/smithing','endreborn:curious_upgrade_template')
+event.add('forge:templates/smithing','deeperdarker:warden_upgrade_smithing_template')
+event.add('forge:templates/smithing','create_additions_synthetics:gilded_upgrade')
+
+// forge:enegergized/items
+event.add('forge:energized/items','create_new_age:overcharged_iron')
+event.add('forge:energized/items','create_new_age:overcharged_golden_sheet')
+event.add('forge:energized/items','create_new_age:overcharged_diamond')
+event.add('forge:energized/items','create_new_age:overcharged_gold')
+event.add('forge:energized/items','create_new_age:overcharged_iron_sheet')
+event.add('forge:energized/items','forge_frontier:overcharged_netherite_ingot')
+
+// forge:enegergized/blocks
+event.add('forge:energized/blocks','forge_frontier:overcharged_iron_block')
+event.add('forge:energized/blocks','forge_frontier:overcharged_gold_block')
+event.add('forge:energized/blocks','forge_frontier:overcharged_diamond_block')
+event.add('forge:energized/blocks','forge_frontier:overcharged_netherite_block')
+
+// create:crushed_raw_materials/piles
+event.add('create:crushed_raw_materials/piles','create_compressed:crushed_iron_pile')
+event.add('create:crushed_raw_materials/piles','create_compressed:crushed_zinc_pile')
+event.add('create:crushed_raw_materials/piles','create_compressed:crushed_copper_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_bismuth_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_calorite_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_desh_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_irradium_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_malachite_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_ostrum_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_tin_pile')
+event.add('create:crushed_raw_materials/piles','forge_frontier:crushed_uranium_pile')
+
+// create:crushed_raw_materials
+event.add('create:crushed_raw_materials','forge_frontier:crushed_desh_ore')
+event.add('create:crushed_raw_materials','forge_frontier:crushed_ostrum_ore')
+event.add('create:crushed_raw_materials','forge_frontier:crushed_calorite_ore')
+event.add('create:crushed_raw_materials','forge_frontier:crushed_bismuth')
+event.add('create:crushed_raw_materials','forge_frontier:crushed_irradium')
+event.add('create:crushed_raw_materials','forge_frontier:crushed_malachite')
+
 })
