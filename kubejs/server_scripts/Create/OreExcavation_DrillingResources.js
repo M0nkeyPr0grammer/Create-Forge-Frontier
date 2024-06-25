db3fed88ad6790cc60d876b6ddd44952c65c798b
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-25 11:50:36 -0500

    Adjusted Gem Ore Vein Drilling Resources

--- kubejs/server_scripts/Create/OreExcavation_DrillingResources.js
+++ kubejs/server_scripts/Create/OreExcavation_DrillingResources.js
@@ -1,135 +1,151 @@
 ServerEvents.recipes(event => {
 // Drill Settings
       // Gem Ores: Lapis, Redstone, Emerald, and Diamond
-        // Lapis
+        // Lapis Ore
         event.custom({
-            type: "createoreexcavation:drilling",
+            type: 'createoreexcavation:drilling',
             drill: {
-            item: "createoreexcavation:drill"
+            item: 'createoreexcavation:drill'
             },
             fluid: {
-                fluid: "create_dd:condense_milk",
+                fluid: 'create_dd:condense_milk',
                 amount: 100
             },
             output: [
             {
-                item: "minecraft:cobblestone",
-                count: 3,
-                chance: 1
-            },
-            {
-                item: "minecraft:cobbled_deepslate",
-                count: 2,
-                chance: .75
-            },
-            {
-                item: "minecraft:lapis_ore",
+                item: 'minecraft:lapis_ore',
                 count: 1, 
+            },
+            {
+                item: 'minecraft:lapis_ore',
+                count: 1,
+                chance: .25
+            },
+            {
+                item: 'create:experience_nugget',
+                count: 1,
                 chance: .50
+            },
+            {
+                item: 'minecraft:cobblestone',
+                count: 1,
+                chance: .10
             }
             ],
             priority: 0,
             stress: 256,
             ticks: 200,
-            vein_id: "createoreexcavation:ore_vein_type/gem_ore"
-        }).id('forge_frontier:drilling/lapis')
+            vein_id: 'createoreexcavation:ore_vein_type/gem_ore'
+        }).id('forge_frontier:drilling/lapis_ore')
         
-        // Redstone
+        // Raw Redstone
         event.custom({
-            type: "createoreexcavation:drilling",
+            type: 'createoreexcavation:drilling',
             drill: {
-            item: "createoreexcavation:drill"
+            item: 'createoreexcavation:drill'
             },
             fluid: {
-                fluid: "create_dd:vanilla",
+                fluid: 'create_dd:vanilla',
                 amount: 100
             },
             output: [
                 {
-                    item: "minecraft:cobblestone",
-                    count: 3,
-                    chance: 1
+                    item: 'createoreexcavation:raw_redstone',
+                    count: 3
                 },
                 {
-                    item: "minecraft:cobbled_deepslate",
-                    count: 2,
-                    chance: .75
+                    item: 'createoreexcavation:raw_redstone',
+                    count: 2, 
+                    chance: .20
                 },
                 {
-                    item: "minecraft:redstone_ore",
-                    count: 1, 
+                    item: 'create:experience_nugget',
+                    count: 1,
                     chance: .50
+                },
+                {
+                    item: 'minecraft:cobblestone',
+                    count: 1,
+                    chance: .10
                 }
             ],
             priority: 0,
             stress: 256,
             ticks: 200,
-            vein_id: "createoreexcavation:ore_vein_type/gem_ore"
-        }).id('forge_frontier:drilling/redstone')
+            vein_id: 'createoreexcavation:ore_vein_type/gem_ore'
+        }).id('forge_frontier:drilling/raw_redstone')
 
-        // Emerald
+        // Raw Emerald
         event.custom({
-            type: "createoreexcavation:drilling",
+            type: 'createoreexcavation:drilling',
             drill: {
-            item: "createoreexcavation:diamond_drill"
+            item: 'createoreexcavation:diamond_drill'
             },
             fluid: {
-                fluid: "create_dd:cream",
+                fluid: 'create_dd:cream',
                 amount: 100
             },
             output: [
                 {
-                    item: "minecraft:cobblestone",
-                    count: 3,
-                    chance: 1
+                    item: 'createoreexcavation:raw_emerald',
+                    count: 3
                 },
                 {
-                    item: "minecraft:cobbled_deepslate",
-                    count: 2,
-                    chance: .75
+                    item: 'createoreexcavation:raw_emerald',
+                    count: 2, 
+                    chance: .20
                 },
                 {
-                    item: "minecraft:emerald_ore",
-                    count: 1, 
+                    item: 'create:experience_nugget',
+                    count: 1,
                     chance: .50
+                },
+                {
+                    item: 'minecraft:cobblestone',
+                    count: 1,
+                    chance: .10
                 }
             ],
             priority: 0,
             stress: 256,
             ticks: 200,
-            vein_id: "createoreexcavation:ore_vein_type/gem_ore"
-        }).id('forge_frontier:drilling/emerald')
+            vein_id: 'createoreexcavation:ore_vein_type/gem_ore'
+        }).id('forge_frontier:drilling/raw_emerald')
 
-        // Diamond
+        // Raw Diamond
         event.custom({
-            type: "createoreexcavation:drilling",
+            type: 'createoreexcavation:drilling',
             drill: {
-            item: "createoreexcavation:diamond_drill"
+            item: 'createoreexcavation:diamond_drill'
             },
             fluid: {
-                fluid: "create_dd:vanilla_milkshake",
+                fluid: 'create_dd:vanilla_milkshake',
                 amount: 100
             },
             output: [
                 {
-                    item: "minecraft:cobblestone",
-                    count: 3,
-                    chance: 1
+                    item: 'createoreexcavation:raw_diamond',
+                    count: 3
                 },
                 {
-                    item: "minecraft:cobbled_deepslate",
-                    count: 2,
-                    chance: .75
+                    item: 'createoreexcavation:raw_diamond',
+                    count: 2, 
+                    chance: .20
                 },
                 {
-                    item: "minecraft:diamond_ore",
-                    count: 1, 
+                    item: 'create:experience_nugget',
+                    count: 1,
                     chance: .50
+                },
+                {
+                    item: 'minecraft:cobblestone',
+                    count: 1,
+                    chance: .10
                 }
             ],
             priority: 0,
             stress: 256,
             ticks: 200,
-            vein_id: "createoreexcavation:ore_vein_type/gem_ore"
-        }).id('forge_frontier:drilling/diamond')
+            vein_id: 'createoreexcavation:ore_vein_type/gem_ore'
+        }).id('forge_frontier:drilling/raw_diamond')
       })
