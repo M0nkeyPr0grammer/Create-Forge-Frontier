139aaa1db8da1d717c9d77574772b06f22cbab91
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-06-26 22:44:01 -0500

    Create Biome tags for Space Ore Vein creations

---
+++ kubejs/server_scripts/Forge Frontier/biome_tags.js
@@ -0,0 +1,9 @@
+ServerEvents.tags("worldgen/biome", (event) => {
+
+// Ad Astra Ore Vein Biome Linkage
+event.add("ad_astra:moon_ore_veins", ["ad_astra:lunar_wastelands"]);
+event.add("ad_astra:mars_ore_veins", ["ad_astra:martian_wastlands"]);
+event.add("ad_astra:venus_ore_veins", ["ad_astra:venus_wastelands",]);
+event.add("ad_astra:glacio_ore_veins", ["ad_astra:glacio_ice_peaks"])
+
+})
