d3f7f8654044383de48d7477e574f56050b7abf7
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-07-01 18:40:09 -0500

    Start of New Custom Planets, with Alex Cave Biomes. Needs adjusting of surface and planet settings

--- kubejs/server_scripts/Forge Frontier/biome_tags.js
+++ kubejs/server_scripts/Forge Frontier/biome_tags.js
@@ -1,5 +1,7 @@
 ServerEvents.tags('worldgen/biome', event => {
 
+  // New Additions
+
     // Ad Astra Ore Vein Biome Linkage
     event.add('ad_astra:moon_ore_veins', 'ad_astra:lunar_wastelands');
     event.add('ad_astra:venus_ore_veins', 'ad_astra:venus_wastelands');
@@ -14,26 +16,28 @@ ServerEvents.tags('worldgen/biome', event => {
     event.add('incendium:structure/has_sanctum', 'ad_astra:glacio_snowy_barrens');
     event.add('incendium:structure/has_reactor', 'ad_astra:venus_wastelands');
 
-    // Removes Old Structure Gen
-    event.remove('cataclysm:has_structure/burning_arena_biomes', 'minecraft:nether_wastes');
-    event.remove('cataclysm:has_structure/soul_black_smith_biomes', [
-      'minecraft:crimson_forest',
-      'minecraft:nether_wastes',
-      'minecraft:soul_sand_valley',
-      'minecraft:warped_forest'
-    ]);
-    event.remove('cataclysm:has_structure/cursed_pyramid_biomes', 'minecraft:desert');
-    event.remove('cataclysm:has_structure/ancient_factory_biomes', '#forge:is_underground');
-    event.remove('incendium:structure/has_pipeline', [
-    'incendium:infernal_dunes',
-    'basalt_deltas',
-    'incendium:toxic_heap'
-    ]);
-    event.remove('incendium:structure/has_sanctum', 'incendium:quartz_flats')
-    event.remove('incendium:structure/has_reactor', [
-        'incendium:volcanic_deltas',
-        'minecraft:basalt_deltas',
-        'incendium:toxic_heap'
-    ]);
+    // Removals
+
+      // Removes Old Structure Gen
+      event.remove('cataclysm:has_structure/burning_arena_biomes', 'minecraft:nether_wastes');
+      event.remove('cataclysm:has_structure/soul_black_smith_biomes', [
+        'minecraft:crimson_forest',
+        'minecraft:nether_wastes',
+        'minecraft:soul_sand_valley',
+        'minecraft:warped_forest'
+      ]);
+      event.remove('cataclysm:has_structure/cursed_pyramid_biomes', 'minecraft:desert');
+      event.remove('cataclysm:has_structure/ancient_factory_biomes', '#forge:is_underground');
+      event.remove('incendium:structure/has_pipeline', [
+      'incendium:infernal_dunes',
+      'basalt_deltas',
+      'incendium:toxic_heap'
+      ]);
+      event.remove('incendium:structure/has_sanctum', 'incendium:quartz_flats')
+      event.remove('incendium:structure/has_reactor', [
+          'incendium:volcanic_deltas',
+          'minecraft:basalt_deltas',
+          'incendium:toxic_heap'
+      ]);
   });
   
