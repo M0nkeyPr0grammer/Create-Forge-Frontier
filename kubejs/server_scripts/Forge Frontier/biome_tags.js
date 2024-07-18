ece262fb19890704c1f5f7d36abfa5f44e5f71b1
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-07-17 23:33:24 -0500

    Fix Scripts

--- kubejs/server_scripts/Forge Frontier/biome_tags.js
+++ kubejs/server_scripts/Forge Frontier/biome_tags.js
@@ -2,51 +2,50 @@ ServerEvents.tags('worldgen/biome', event => {
 
   // New Additions
 
-    // Ad Astra Ore Vein Biome Linkage
-    event.add('ad_astra:moon_ore_veins', 'ad_astra:lunar_wastelands');
-    event.add('ad_astra:venus_ore_veins', 'ad_astra:venus_wastelands');
-    event.add('ad_astra:glacio_ore_veins', 'ad_astra:glacio_ice_peaks');
-  
-    // Adds New Structure Gen
-    event.add('cataclysm:has_structure/burning_arena_biomes', 'ad_astra:mercury_deltas');
-    event.add('cataclysm:has_structure/soul_black_smith_biomes', 'ad_astra:martian_polar_caps');
-    event.add('cataclysm:has_structure/cursed_pyramid_biomes', 'ad_astra:venus_wastelands');
-    event.add('cataclysm:has_structure/ancient_factory_biomes', 'ad_astra:glacio_ice_peaks');
-    event.add('incendium:structure/has_pipeline', 'ad_astra:lunar_wastelands');
-    event.add('incendium:structure/has_sanctum', 'ad_astra:glacio_snowy_barrens');
-    event.add('incendium:structure/has_reactor', 'ad_astra:venus_wastelands');
+  // Ad Astra Ore Vein Biome Linkage
+  event.add('ad_astra:moon_ore_veins', 'ad_astra:lunar_wastelands');
+  event.add('ad_astra:venus_ore_veins', 'ad_astra:venus_wastelands');
+  event.add('ad_astra:glacio_ore_veins', 'ad_astra:glacio_ice_peaks');
 
-    // Removals
+  // Adds New Structure Gen
+  event.add('cataclysm:has_structure/burning_arena_biomes', 'ad_astra:mercury_deltas');
+  event.add('cataclysm:has_structure/soul_black_smith_biomes', 'ad_astra:martian_polar_caps');
+  event.add('cataclysm:has_structure/cursed_pyramid_biomes', 'ad_astra:venus_wastelands');
+  event.add('cataclysm:has_structure/ancient_factory_biomes', 'ad_astra:glacio_ice_peaks');
+  event.add('incendium:structure/has_pipeline', 'ad_astra:lunar_wastelands');
+  event.add('incendium:structure/has_sanctum', 'ad_astra:glacio_snowy_barrens');
+  event.add('incendium:structure/has_reactor', 'ad_astra:venus_wastelands');
 
-      // Remove Alex's Caves from Overworld Biomes
-      event.remove("forge:is_sparse/overworld", [
-        "alexscaves:magnetic_caves",
-        "alexscaves:toxic_caves",
-        "alexscaves:forlorn_hollows",
-      ]);
-      event.remove("forge:is_wasteland", "alexscaves:toxic_caves");
-      event.remove("forge:is_spooky", "alexscaves:forlorn_hollows");
+  // Removals
 
-      // Removes Old Structure Gen
-      event.remove('cataclysm:has_structure/burning_arena_biomes', 'minecraft:nether_wastes');
-      event.remove('cataclysm:has_structure/soul_black_smith_biomes', [
-        'minecraft:crimson_forest',
-        'minecraft:nether_wastes',
-        'minecraft:soul_sand_valley',
-        'minecraft:warped_forest'
-      ]);
-      event.remove('cataclysm:has_structure/cursed_pyramid_biomes', 'minecraft:desert');
-      event.remove('cataclysm:has_structure/ancient_factory_biomes', '#forge:is_underground');
-      event.remove('incendium:structure/has_pipeline', [
-      'incendium:infernal_dunes',
-      'basalt_deltas',
-      'incendium:toxic_heap'
-      ]);
-      event.remove('incendium:structure/has_sanctum', 'incendium:quartz_flats')
-      event.remove('incendium:structure/has_reactor', [
-          'incendium:volcanic_deltas',
-          'minecraft:basalt_deltas',
-          'incendium:toxic_heap'
-      ]);
-  });
-  
+  // Remove Alex's Caves from Overworld Biomes
+  event.remove('forge:is_sparse/overworld', [
+    'alexscaves:magnetic_caves',
+    'alexscaves:toxic_caves',
+    'alexscaves:forlorn_hollows'
+  ]);
+  event.remove('forge:is_wasteland', 'alexscaves:toxic_caves');
+  event.remove('forge:is_spooky', 'alexscaves:forlorn_hollows');
+
+  // Removes Old Structure Gen
+  event.remove('cataclysm:has_structure/burning_arena_biomes', 'minecraft:nether_wastes');
+  event.remove('cataclysm:has_structure/soul_black_smith_biomes', [
+    'minecraft:crimson_forest',
+    'minecraft:nether_wastes',
+    'minecraft:soul_sand_valley',
+    'minecraft:warped_forest'
+  ]);
+  event.remove('cataclysm:has_structure/cursed_pyramid_biomes', 'minecraft:desert');
+  event.remove('cataclysm:has_structure/ancient_factory_biomes', '#forge:is_underground');
+  event.remove('incendium:structure/has_pipeline', [
+    'incendium:infernal_dunes',
+    'minecraft:basalt_deltas',
+    'incendium:toxic_heap'
+  ]);
+  event.remove('incendium:structure/has_sanctum', 'incendium:quartz_flats');
+  event.remove('incendium:structure/has_reactor', [
+    'incendium:volcanic_deltas',
+    'minecraft:basalt_deltas',
+    'incendium:toxic_heap'
+  ]);
+});
