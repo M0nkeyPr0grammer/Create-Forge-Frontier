985e630fec103f8fa3cf40b047bf9b6fc6f15579
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-07-23 00:23:10 -0500

    Creates Compressed Versions of Malachite for Progression

--- kubejs/startup_scripts/Forge Frontier/CustomItems.js
+++ kubejs/startup_scripts/Forge Frontier/CustomItems.js
@@ -1,5 +1,26 @@
 
 StartupEvents.registry('block', event => {
+    // Compressed Blocks
+    // Malachite
+    function createCompressedMalachite(event, level) {
+        event.create(`forge_frontier:compressed_malachite_${level}x`)
+            .displayName(`Compressed Malachite ${level}x`)
+            .soundType('metal')
+            .mapColor('stone')
+            .hardness(2.0)
+            .model(`forge_frontier:block/compressed_blocks/compressed_malachite_${level}x`)
+            .requiresTool(true)
+            .renderType('solid')
+            .tagItem('tooltiprareness:common_item')
+            .tagBlock('mineable/pickaxe')
+            .fullBlock(true);
+    }
+    
+    for (let i = 1; i <= 9; i++) {
+        createCompressedMalachite(event, i);
+    }
+
+    // Overcharged Blocks
     event.create('forge_frontier:overcharged_iron_block')
         .displayName('Overcharged Iron Block')
         .soundType('metal') 
