f0858b8d3075c2be3e36bdf0ed8256eb733b13f2
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-07-29 16:50:58 -0500

    Created Recipes for Remaining Drills, adding in Drill Progression

--- kubejs/server_scripts/Create/Drill_Head_Recipes.js
+++ kubejs/server_scripts/Create/Drill_Head_Recipes.js
@@ -72,9 +72,129 @@ ServerEvents.recipes(event => {
             {
                 A: 'minecraft:dragon_head',
                 B: 'minecraft:dragon_breath',
-                C: 'createoreexcavation:calorite_drill',
+                C: 'createoreexcavation:echo_drill',
                 D: 'quark:dragon_scale'
             }
         ).id( 'forge_frontier:shaped/dragon_drill' )
+
+        // Amber Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:amber_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'alexscaves:amber_curiosity',
+                B: 'alexscaves:amber',
+                C: 'createoreexcavation:guano_drill'
+            }
+        ).id( 'forge_frontier:shaped/amber_drill' )
+
+        // Azure Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:azure_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'alexscaves:block_of_azure_neodymium',
+                B: 'alexscaves:azure_neodymium_ingot',
+                C: 'createoreexcavation:amber_drill'
+            }
+        ).id( 'forge_frontier:shaped/azure_drill' )
+
+        // Guano Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:guano_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'alexscaves:guano_block',
+                B: 'alexscaves:guano',
+                C: 'createoreexcavation:pearl_drill'
+            }
+        ).id( 'forge_frontier:shaped/guano_drill' )
+
+        // Pearl Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:pearl_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'alexscaves:block_of_pearl',
+                B: 'alexscaves:pearl',
+                C: 'createoreexcavation:dragon_drill'
+            }
+        ).id( 'forge_frontier:shaped/pearl_drill' )
+
+        // Scarlet Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:scarlet_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'alexscaves:block_of_scarlet_neodymium',
+                B: 'alexscaves:scarlet_neodymium_ingot',
+                C: 'createoreexcavation:azure_drill'
+            }
+        ).id( 'forge_frontier:shaped/scarlet_drill' )
+
+        // Sulfur Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:sulfur_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'alexscaves:sulfur',
+                B: 'alexscaves:sulfur_dust',
+                C: 'createoreexcavation:scarlet_drill'
+            }
+        ).id( 'forge_frontier:shaped/sulfur_drill' )
+
+        // Uranium Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:uranium_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'alexscaves:block_of_uranium',
+                B: 'alexscaves:uranium',
+                C: 'createoreexcavation:sulfur_drill'
+            }
+        ).id( 'forge_frontier:shaped/uranium_drill' )
+
+        // Echo Drill Head
+        event.shaped(
+            Item.of('createoreexcavation:echo_drill'),
+            [
+                'AB ',
+                'BCB',
+                ' BB'
+            ],
+            {
+                A: 'deeperdarker:heart_of_the_deep',
+                B: 'minecraft:echo_shard',
+                C: 'createoreexcavation:calorite_drill'
+            }
+        ).id( 'forge_frontier:shaped/echo_drill' )
     
 })
