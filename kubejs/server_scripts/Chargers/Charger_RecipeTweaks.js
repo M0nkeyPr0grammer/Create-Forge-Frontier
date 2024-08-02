0dfb60c1e9178a501981b8d09139c08d057c2721
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-08-02 00:27:34 -0500

    Changed Chargers Textures, and updated recipes

--- kubejs/server_scripts/Chargers/Charger_RecipeTweaks.js
+++ kubejs/server_scripts/Chargers/Charger_RecipeTweaks.js
@@ -4,71 +4,79 @@ ServerEvents.recipes(event => {
 	// Shaped Recipes
 		// Charger Tier I
 		event.remove({ id: 'chargers:charger_t1' })
-		event.shaped(
-			Item.of('chargers:charger_t1'),
-			[
-				'ABA',
-				'ACA',
-				'ADA'
+		event.custom({
+			type: 'create:item_application',
+			ingredients: [
+			  {
+				item: 'createaddition:tesla_coil'
+			  },
+			  {
+				item: 'forge_frontier:overcharged_iron_block'
+			  }
 			],
-			{
-				A: 'create_new_age:overcharged_iron',
-				B: 'minecraft:redstone',
-				C: 'minecraft:redstone_block',
-				D: 'createaddition:tesla_coil'
-			}
-		).id('forge_frontier:shaped/charger_t1')
+			results: [
+			  {
+				item: 'chargers:charger_t1'
+			  }
+			]
+		  }).id('forge_frontier:item_application/charger_t1');
 
 		// Charger Tier II
 		event.remove({ id: 'chargers:charger_t2' }) 
-		event.shaped(
-			Item.of('chargers:charger_t2'),
-			[
-				'ABA',
-				'ACA',
-				'ADA'
+		event.custom({
+			type: 'create:item_application',
+			ingredients: [
+			  {
+				item: 'chargers:charger_t1'
+			  },
+			  {
+				item: 'forge_frontier:overcharged_gold_block'
+			  }
 			],
-			{
-				A: 'create_new_age:overcharged_gold',
-				B: 'minecraft:redstone',
-				C: 'minecraft:redstone_block',
-				D: 'chargers:charger_t1'
-			}
-		).id('forge_frontier:shaped/charger_t2')
+			results: [
+			  {
+				item: 'chargers:charger_t2'
+			  }
+			]
+		  }).id('forge_frontier:item_application/charger_t2');
 
 		// Charger Tier III
 		event.remove({ id: 'chargers:charger_t3' }) 
-		event.shaped(
-			Item.of('chargers:charger_t3'),
-			[
-				'ABA',
-				'ACA',
-				'ADA'
+		event.custom({
+			type: 'create:item_application',
+			ingredients: [
+			  {
+				item: 'chargers:charger_t2'
+			  },
+			  {
+				item: 'forge_frontier:overcharged_diamond_block'
+			  }
 			],
-			{
-				A: 'create_new_age:overcharged_diamond',
-				B: 'minecraft:redstone',
-				C: 'minecraft:redstone_block',
-				D: 'chargers:charger_t2'
-			}
-		).id('forge_frontier:shaped/charger_t3')
+			results: [
+			  {
+				item: 'chargers:charger_t3'
+			  }
+			]
+		  }).id('forge_frontier:item_application/charger_t3');
 
 		// Charger Tier IV
 		event.remove({ id: 'chargers:charger_t4' }) 
-		event.shaped(
-			Item.of('chargers:charger_t4'),
-			[
-				'ABA',
-				'ACA',
-				'ADA'
+		event.custom({
+			type: 'create:item_application',
+			ingredients: [
+			  {
+				item: 'chargers:charger_t3'
+			  },
+			  {
+				item: 'forge_frontier:overcharged_netherite_block'
+			  }
 			],
-			{
-				A: 'forge_frontier:overcharged_netherite_ingot',
-				B: 'minecraft:redstone',
-				C: 'minecraft:redstone_block',
-				D: 'chargers:charger_t3'
-			}
-		).id('forge_frontier:shaped/charger_t4')
+			results: [
+			  {
+				item: 'chargers:charger_t4'
+			  }
+			]
+		  }).id('forge_frontier:item_application/charger_t4');
 
 		// Wireless Charger
 		event.remove({ id: 'chargers:wireless_charger' })  
