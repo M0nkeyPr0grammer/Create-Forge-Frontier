557eba6a5aa214bdf745db84f39663757c35f3ca
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-08-22 01:04:59 -0500

    Added Check for Welcome Message, so it only shows first world load

--- kubejs/server_scripts/Forge Frontier/WorldLaunchMessage.js
+++ kubejs/server_scripts/Forge Frontier/WorldLaunchMessage.js
@@ -1,11 +1,16 @@
 // Listen for the player join event
 PlayerEvents.loggedIn((event) => {
   const player = event.player;
+  const welcomeTag = 'receivedWelcomeMessage'; // Tag to track if the player has received the welcome message
 
-  // Send a welcome message in bold
-  player.tell("Welcome to Create: Forge Frontier, Pioneer!\n");
-  player.tell("Embark on an epic journey and explore diverse landscapes. May your adventures be thrilling and your Create factories be extraordinary!\n");
-  player.tell("- Lead Developer, M0nkeyPr0grammer")
+  // Check if the player has already received the welcome message
+  if (!player.persistentData[welcomeTag]) {
+    // Send a welcome message in bold
+    player.tell("§lWelcome to Create - Forge Frontier, Pioneer!\n");
+    player.tell("Embark on an epic journey and explore diverse landscapes. May your adventures be thrilling and your Create factories be extraordinary!\n");
+    player.tell("- Lead Developer, M0nkeyPr0grammer");
 
-
+    // Mark that the player has received the welcome message
+    player.persistentData[welcomeTag] = true;
+  }
 });
