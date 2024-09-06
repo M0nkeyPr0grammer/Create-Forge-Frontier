cd018fd486e3e024d03a84939c46027eece45b5b
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-09-05 23:44:42 -0500

    Cleaned Up Fluid Creation to make adding future fluids easier

--- kubejs/startup_scripts/Forge Frontier/CustomFluids.js
+++ kubejs/startup_scripts/Forge Frontier/CustomFluids.js
@@ -1,314 +1,58 @@
-StartupEvents.registry('fluid',event => {
+StartupEvents.registry('fluid', event => {
 
-    // Creates Molten Lead
-    event.create('forge_frontier:molten_lead')
-        .color(0x5c7376)
-        .displayName('Molten Lead')
-        .bucketColor(0x5c7376)
-        .thickTexture(0x5c7376)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
+    // List of molten fluids with their properties
+    const moltenFluids = [
+        { name: 'lead', color: 0x5c7376 },
+        { name: 'aluminum', color: 0xffffff },
+        { name: 'copper', color: 0xc75d34 },
+        { name: 'iron', color: 0xe6c6af },
+        { name: 'gold', color: 0xf4c230 },
+        { name: 'carbon', color: 0x2d2d2d },
+        { name: 'diamond', color: 0x49ead6 },
+        { name: 'zinc', color: 0xcfe9d4 },
+        { name: 'brass', color: 0x8a6d3c },
+        { name: 'emerald', color: 0x1dba59 },
+        { name: 'redstone', color: 0xac2727 },
+        { name: 'lapis', color: 0x1c52a6 },
+        { name: 'ancient_debris', color: 0x412e29 },
+        { name: 'netherite', color: 0x4b4042 },
+        { name: 'bone_marrow', color: 0xc7c3a5 },
+        { name: 'netherite_iron', color: 0xa6a6a6 },
+        { name: 'netherite_gold', color: 0xf9ee3e },
+        { name: 'netherite_emerald', color: 0x007a18 },
+        { name: 'netherite_diamond', color: 0x327b81 },
+        { name: 'bronze', color: 0xc37b5c },
+        { name: 'tin', color: 0xe7e8ed },
+        { name: 'quartz', color: 0x989186 },
+        { name: 'ice_shard', color: 0x8890b8 },
+        { name: 'uranium', color: 0x00fc00 },
+        { name: 'bismuth', color: 0x555341 },
+        { name: 'irradium', color: 0x21532b },
+        { name: 'malachite', color: 0x3c776d },
+        { name: 'steel', color: 0x757276 },
+        { name: 'desh', color: 0xb05a3c },
+        { name: 'calorite', color: 0xb63044 },
+        { name: 'ostrum', color: 0x76525f }
+    ];
 
-    // Creates Molten Aluminum
-    event.create('forge_frontier:molten_aluminum')
-        .color(0xffffff)
-        .displayName('Molten Aluminum')
-        .bucketColor(0xffffff)
-        .thickTexture(0xffffff)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
+    // Function to create molten fluids
+    function createMoltenFluid(event, name, color) {
+        event.create(`forge_frontier:molten_${name}`)
+            .color(color)
+            .displayName(`Molten ${capitalize(name)}`)
+            .bucketColor(color)
+            .thickTexture(color)
+            .viscosity(6000)
+            .temperature(1300)
+            .bucketItem;
+    }
 
-    // Creates Molten Copper
-    event.create('forge_frontier:molten_copper')
-        .color(0xc75d34)
-        .displayName('Molten Copper')
-        .bucketColor(0xc75d34)
-        .thickTexture(0xc75d34)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
+    // Helper function to capitalize names
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
 
-    // Creates Molten Iron
-    event.create('forge_frontier:molten_iron')
-        .color(0xe6c6af)
-        .displayName('Molten Iron')
-        .bucketColor(0xe6c6af)
-        .thickTexture(0xe6c6af)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Gold 
-    event.create('forge_frontier:molten_gold')
-        .color(0xf4c230)
-        .displayName('Molten Gold')
-        .bucketColor(0xf4c230)
-        .thickTexture(0xf4c230)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Carbon
-    event.create('forge_frontier:molten_carbon')
-        .color(0x2d2d2d)
-        .displayName('Molten Carbon')
-        .bucketColor(0x2d2d2d)
-        .thickTexture(0x2d2d2d)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Diamond
-    event.create('forge_frontier:molten_diamond')
-        .color(0x49ead6)
-        .displayName('Molten Diamond')
-        .bucketColor(0x49ead6)
-        .thickTexture(0x49ead6)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Zinc
-    event.create('forge_frontier:molten_zinc')
-        .color(0xcfe9d4)
-        .displayName('Molten Zinc')
-        .bucketColor(0xcfe9d4)
-        .thickTexture(0xcfe9d4)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Brass
-    event.create('forge_frontier:molten_brass')
-        .color(0x8a6d3c)
-        .displayName('Molten Brass')
-        .bucketColor(0x8a6d3c)
-        .thickTexture(0x8a6d3c)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem; 
-
-    // Creates Molten Emerald
-    event.create('forge_frontier:molten_emerald')
-        .color(0x1dba59)
-        .displayName('Molten Emerald')
-        .bucketColor(0x1dba59)
-        .thickTexture(0x1dba59)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem; 
-
-    // Creates Molten Redstone
-    event.create('forge_frontier:molten_redstone')
-        .color(0xac2727)
-        .displayName('Molten Redstone')
-        .bucketColor(0xac2727)
-        .thickTexture(0xac2727)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem; 
-
-    // Creates Molten Lapis
-    event.create('forge_frontier:molten_lapis')
-        .color(0x1c52a6)
-        .displayName('Molten Lapis')
-        .bucketColor(0x1c52a6)
-        .thickTexture(0x1c52a6)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem; 
-
-    // Creates Molten Ancient Debris
-    event.create('forge_frontier:molten_ancient_debris')
-        .color(0x412e29)
-        .displayName('Molten Ancient Debris')
-        .bucketColor(0x412e29)
-        .thickTexture(0x412e29)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem; 
-
-    // Creates Molten Netherite
-    event.create('forge_frontier:molten_netherite')
-        .color(0x4b4042)
-        .displayName('Molten Netherite')
-        .bucketColor(0x4b4042)
-        .thickTexture(0x4b4042)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem; 
-
-    // Creates Molten Bone Marrow
-    event.create('forge_frontier:molten_bone_marrow')
-        .color(0xc7c3a5)
-        .displayName('Molten Bone Marrow')
-        .bucketColor(0xc7c3a5)
-        .thickTexture(0xc7c3a5)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Netherite Iron
-    event.create('forge_frontier:molten_netherite_iron')
-        .color(0xa6a6a6)
-        .displayName('Molten Netherite-Iron')
-        .bucketColor(0xa6a6a6)
-        .thickTexture(0xa6a6a6)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Netherite Gold
-    event.create('forge_frontier:molten_netherite_gold')
-        .color(0xf9ee3e)
-        .displayName('Molten Netherite-Gold')
-        .bucketColor(0xf9ee3e)
-        .thickTexture(0xf9ee3e)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Netherite Emerald
-    event.create('forge_frontier:molten_netherite_emerald')
-        .color(0x007a18)
-        .displayName('Molten Netherite-Emerald')
-        .bucketColor(0x007a18)
-        .thickTexture(0x007a18)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Netherite Diamond
-    event.create('forge_frontier:molten_netherite_diamond')
-        .color(0x327b81)
-        .displayName('Molten Netherite-Diamond')
-        .bucketColor(0x327b81)
-        .thickTexture(0x327b81)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-
-    // Creates Molten Bronze
-    event.create('forge_frontier:molten_bronze')
-        .color(0xc37b5c)
-        .displayName('Molten Bronze')
-        .bucketColor(0xc37b5c)
-        .thickTexture(0xc37b5c)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Tin    
-    event.create('forge_frontier:molten_tin')
-        .color(0xe7e8ed)
-        .displayName('Molten Tin')
-        .bucketColor(0xe7e8ed)
-        .thickTexture(0xe7e8ed)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-    
-    // Creates Molten Quartz
-    event.create('forge_frontier:molten_quartz')
-        .color(0x989186)
-        .displayName('Molten Quartz')
-        .bucketColor(0x989186)
-        .thickTexture(0x989186)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Ice Shard
-    event.create('forge_frontier:molten_ice_shard')
-        .color(0x8890b8)
-        .displayName('Molten Ice Shard')
-        .bucketColor(0x8890b8)
-        .thickTexture(0x8890b8)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Uranium
-    event.create('forge_frontier:molten_uranium')
-        .color(0x00fc00)
-        .displayName('Molten Uranium')
-        .bucketColor(0x00fc00)
-        .thickTexture(0x00fc00)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Bismuth
-    event.create('forge_frontier:molten_bismuth')
-        .color(0x555341)
-        .displayName('Molten Bismuth')
-        .bucketColor(0x555341)
-        .thickTexture(0x555341)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Creates Molten Irradium
-    event.create('forge_frontier:molten_irradium')
-        .color(0x21532b)
-        .displayName('Molten Irradium')
-        .bucketColor(0x21532b)
-        .thickTexture(0x21532b)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Create Molten Malachite
-    event.create('forge_frontier:molten_malachite')
-        .color(0x3c776d)
-        .displayName('Molten Malachite')
-        .bucketColor(0x3c776d)
-        .thickTexture(0x3c776d)
-        .viscosity(6000)  
-        .temperature(1300)  
-        .bucketItem;  
-
-    // Molten Steel
-    event.create('forge_frontier:molten_steel')
-        .color(0x757276)
-        .displayName('Molten Steel')
-        .bucketColor(0x757276)
-        .thickTexture(0x757276)
-        .viscosity(6000)
-        .temperature(1300)
-        .bucketItem;
-
-    // Molten Desh
-    event.create('forge_frontier:molten_desh')
-        .color(0xb05a3c)
-        .displayName('Molten Desh')
-        .bucketColor(0xb05a3c)
-        .thickTexture(0xb05a3c)
-        .viscosity(6000)
-        .temperature(1300)
-        .bucketItem;
-
-    // Molten Calorite
-    event.create('forge_frontier:molten_calorite')
-        .color(0xb63044)
-        .displayName('Molten Calorite')
-        .bucketColor(0xb63044)
-        .thickTexture(0xb63044)
-        .viscosity(6000)
-        .temperature(1300)
-        .bucketItem;
-
-    // Molten Ostrum
-    event.create('forge_frontier:molten_ostrum')
-        .color(0x76525f)
-        .displayName('Molten Ostrum')
-        .bucketColor(0x76525f)
-        .thickTexture(0x76525f)
-        .viscosity(6000)
-        .temperature(1300)
-        .bucketItem;
+    // Create all molten fluids
+    moltenFluids.forEach(fluid => createMoltenFluid(event, fluid.name, fluid.color));
 
 });
