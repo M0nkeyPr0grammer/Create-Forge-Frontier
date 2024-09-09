2eef7114c3d82dc9dd3f797852815a32dbe78b8d
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-09-09 00:27:06 -0500

    Cleaned Up Molding Recipes for Easier addition of new mold recipes

--- kubejs/server_scripts/Mold Recipes/CoalCompactingMold_Recipes.js
+++ kubejs/server_scripts/Mold Recipes/CoalCompactingMold_Recipes.js
@@ -1,57 +1,46 @@
 ServerEvents.recipes(event => {
 
-// Charcoal Mold Recipe
-event.custom({
-    type: 'create:compacting',
-    ingredients: [
-        {
-            fluid: 'forge_frontier:molten_carbon',
-            amount: 100
-        },
-        {
-            item: 'forge_frontier:coal_mold',
-            count: 1
-        },
-        {
-            tag: 'minecraft:logs',
-            count: 1
+    // Reusable function for compacting recipes
+    function createCompactingRecipe(fluid, resultItem, recipeName, additionalIngredient, fluidAmount) {
+        if (fluidAmount === undefined) {
+            fluidAmount = 100; // Default fluid amount
         }
-    ],
-    results: [
-        {
-            item: 'minecraft:charcoal',
-            count: 1
-        },
-        {
-            item: 'forge_frontier:coal_mold'
-        }
-    ],
-    heatRequirement: 'superheated'
-}).id('forge_frontier:compacting/charcoal')
 
-// Coal Mold Recipe
-event.custom({
-    type: 'create:compacting',
-    ingredients: [
-        {
-            fluid: 'forge_frontier:molten_carbon',
-            amount: 100
-        },
-        {
-            item: 'forge_frontier:coal_mold',
-            count: 1
-        }
-    ],
-    results: [
-        {
-            item: 'minecraft:coal',
-            count: 1
-        },
-        {
-            item: 'forge_frontier:coal_mold'
-        }
-    ],
-    heatRequirement: 'superheated'
-}).id('forge_frontier:compacting/coal')
+        const ingredients = [
+            { fluid: fluid, amount: fluidAmount },
+            { item: 'forge_frontier:coal_mold', count: 1 }
+        ];
 
-})
+        // Add additional ingredient if provided
+        if (additionalIngredient) {
+            ingredients.push(additionalIngredient);
+        }
+
+        event.custom({
+            type: 'create:compacting',
+            ingredients: ingredients,
+            results: [
+                { item: resultItem, count: 1 },
+                { item: 'forge_frontier:coal_mold' }
+            ],
+            heatRequirement: 'superheated'
+        }).id(`forge_frontier:compacting/${recipeName}`);
+    }
+
+    // Create charcoal mold recipe with an additional ingredient
+    createCompactingRecipe(
+        'forge_frontier:molten_carbon', 
+        'minecraft:charcoal', 
+        'charcoal', 
+        { tag: 'minecraft:logs', count: 1 }
+    );
+
+    // Create coal mold recipe without an additional ingredient
+    createCompactingRecipe(
+        'forge_frontier:molten_carbon', 
+        'minecraft:coal', 
+        'coal', 
+        null
+    );
+
+});
--- kubejs/server_scripts/Mold Recipes/UpgradeMoldCompactingRecipes.js
+++ kubejs/server_scripts/Mold Recipes/UpgradeMoldCompactingRecipes.js
@@ -1,151 +1,28 @@
 ServerEvents.recipes(event => {
     // Remove Vanilla Smithing Template Recipe
-    event.remove({ id: 'minecraft:netherite_upgrade_smithing_template'})
-    
-    // Netherite Upgrade Template Recipe
-    event.custom({
-        type:"create:compacting",
-        ingredients: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                fluid: 'forge_frontier:molten_diamond',
-                amount: 400
-            },
-            {
-                item: 'minecraft:netherrack',
-                amount: 1
-            }
-        ],
-        results: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                item: 'minecraft:netherite_upgrade_smithing_template'
-            }
-        ], 
-        heatRequirement: 'superheated'
-    }).id('forge_frontier:compacting/netherite_upgrade')
+    event.remove({ id: 'minecraft:netherite_upgrade_smithing_template' });
 
-    // Netherite-Iron Upgrade Template
-    event.custom({
-        type:"create:compacting",
-        ingredients: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                fluid: 'forge_frontier:molten_iron',
-                amount: 400
-            },
-            {
-                item: 'minecraft:netherite_ingot',
-                amount: 1
-            }
-        ],
-        results: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                item: 'forge_frontier:netherite_iron_upgrade'
-            }
-        ], 
-        heatRequirement: 'superheated'
-    }).id('forge_frontier:compacting/netherite_iron_upgrade')
+    // Reusable function for compacting upgrade template recipes
+    const createUpgradeCompactingRecipe = (moldItem, fluid, fluidAmount, additionalItem, resultItem, recipeName) => {
+        event.custom({
+            type: 'create:compacting',
+            ingredients: [
+                { item: moldItem, amount: 1 },
+                { fluid: fluid, amount: fluidAmount },
+                { item: additionalItem, amount: 1 }
+            ],
+            results: [
+                { item: moldItem, amount: 1 },
+                { item: resultItem }
+            ],
+            heatRequirement: 'superheated'
+        }).id(`forge_frontier:compacting/${recipeName}`);
+    };
 
-    // Netherite-Gold Upgrade Template
-    event.custom({
-        type:"create:compacting",
-        ingredients: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                fluid: 'forge_frontier:molten_gold',
-                amount: 400
-            },
-            {
-                item: 'advancednetherite:netherite_iron_ingot',
-                amount: 1
-            }
-        ],
-        results: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                item: 'forge_frontier:netherite_gold_upgrade'
-            }
-        ], 
-        heatRequirement: 'superheated'
-    }).id('forge_frontier:compacting/netherite_gold_upgrade')
-
-    // Netherite-Emerald Template
-    event.custom({
-        type:"create:compacting",
-        ingredients: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                fluid: 'forge_frontier:molten_emerald',
-                amount: 400
-            },
-            {
-                item: 'advancednetherite:netherite_gold_ingot',
-                amount: 1
-            }
-        ],
-        results: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                item: 'forge_frontier:netherite_emerald_upgrade'
-            }
-        ], 
-        heatRequirement: 'superheated'
-    }).id('forge_frontier:compacting/netherite_emerald_upgrade')
-
-    // Netherite-Diamond Upgrade
-    event.custom({
-        type:"create:compacting",
-        ingredients: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                fluid: 'forge_frontier:molten_diamond',
-                amount: 400
-            },
-            {
-                item: 'advancednetherite:netherite_emerald_ingot',
-                amount: 1
-            }
-        ],
-        results: [
-            {
-                item: 'forge_frontier:upgrade_mold',
-                amount: 1
-            },
-            {
-                item: 'forge_frontier:netherite_diamond_upgrade'
-            }
-        ], 
-        heatRequirement: 'superheated'
-    }).id('forge_frontier:compacting/netherite_diamond_upgrade')
-
-
-})
+    // Recipes
+    createUpgradeCompactingRecipe('forge_frontier:upgrade_mold', 'forge_frontier:molten_diamond', 400, 'minecraft:netherrack', 'minecraft:netherite_upgrade_smithing_template', 'netherite_upgrade');
+    createUpgradeCompactingRecipe('forge_frontier:upgrade_mold', 'forge_frontier:molten_iron', 400, 'minecraft:netherite_ingot', 'forge_frontier:netherite_iron_upgrade', 'netherite_iron_upgrade');
+    createUpgradeCompactingRecipe('forge_frontier:upgrade_mold', 'forge_frontier:molten_gold', 400, 'advancednetherite:netherite_iron_ingot', 'forge_frontier:netherite_gold_upgrade', 'netherite_gold_upgrade');
+    createUpgradeCompactingRecipe('forge_frontier:upgrade_mold', 'forge_frontier:molten_emerald', 400, 'advancednetherite:netherite_gold_ingot', 'forge_frontier:netherite_emerald_upgrade', 'netherite_emerald_upgrade');
+    createUpgradeCompactingRecipe('forge_frontier:upgrade_mold', 'forge_frontier:molten_diamond', 400, 'advancednetherite:netherite_emerald_ingot', 'forge_frontier:netherite_diamond_upgrade', 'netherite_diamond_upgrade');
+});
