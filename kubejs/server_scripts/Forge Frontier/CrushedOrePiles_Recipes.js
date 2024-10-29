46b8a309398227113b037db2ede2823b0d3abba5
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2024-10-29 14:51:23 -0500

    Created Crushed Pile Recipes

--- kubejs/server_scripts/Forge Frontier/CrushedOrePiles_Recipes.js
+++ kubejs/server_scripts/Forge Frontier/CrushedOrePiles_Recipes.js
@@ -1,277 +1,59 @@
 // Custom Crushed Ore Piles Shaped and Shapeless Recipes
 ServerEvents.recipes(event => {
 
-// Shapeless Recipes
+    const ores = [
+        { pile: 'crushed_lead_pile', raw: 'create:crushed_raw_lead' },
+        { pile: 'crushed_aluminum_pile', raw: 'create:crushed_raw_aluminum' },
+        { pile: 'crushed_bismuth_pile', raw: 'forge_frontier:crushed_bismuth' },
+        { pile: 'crushed_calorite_pile', raw: 'forge_frontier:crushed_calorite' },
+        { pile: 'crushed_desh_pile', raw: 'forge_frontier:crushed_desh' },
+        { pile: 'crushed_irradium_pile', raw: 'forge_frontier:crushed_irradium' },
+        { pile: 'crushed_malachite_pile', raw: 'forge_frontier:crushed_malachite' },
+        { pile: 'crushed_ostrum_pile', raw: 'forge_frontier:crushed_ostrum' },
+        { pile: 'crushed_tin_pile', raw: 'create:crushed_raw_tin' },
+        { pile: 'crushed_uranium_pile', raw: 'create:crushed_raw_uranium' },
+		{ pile: 'crushed_verdantine_pile', raw: 'forge_frontier:crushed_verdantine'},
+		{ pile: 'crushed_glacium_pile', raw: 'forge_frontier:crushed_glacium'},
+		{ pile: 'crushed_debris_pile', raw: 'forge_frontier:crushed_debris'},
+		{ pile: 'crushed_grime_pile', raw: 'forge_frontier:crushed_grime'},
+		{ pile: 'crushed_aubrum_pile', raw: 'forge_frontier:crushed_aubrum'},
+		{ pile: 'crushed_pyroclast_pile', raw: 'forge_frontier:crushed_pyroclast'},
+		{ pile: 'crushed_frostite_pile', raw: 'forge_frontier:crushed_frostite'},
+		{ pile: 'crushed_stellaris_pile', raw: 'forge_frontier:crushed_stellaris'},
+		{ pile: 'crushed_radiantite_pile', raw: 'forge_frontier:crushed_radiantite'},
+		{ pile: 'crushed_shale_pile', raw: 'forge_frontier:crushed_shale'},
+		{ pile: 'crushed_palerock_pile', raw: 'forge_frontier:crushed_palerock'},
+		{ pile: 'crushed_guanite_pile', raw: 'forge_frontier:crushed_guanite'},
+		{ pile: 'crushed_pearlyte_pile', raw: 'forge_frontier:crushed_pearlyte'},
+		{ pile: 'crushed_amberlite_pile', raw: 'forge_frontier:crushed_amberlite'},
+		{ pile: 'crushed_azurnium_pile', raw: 'forge_frontier:crushed_azurnium'},
+		{ pile: 'crushed_neodymrium_pile', raw: 'forge_frontier:crushed_neodymrium'},
+		{ pile: 'crushed_uraniumnite_pile', raw: 'forge_frontier:crushed_uraniumnite'},
+		{ pile: 'crushed_sulphite_pile', raw: 'forge_frontier:crushed_sulphite'},
+    ];
 
-	// Lead Pile to Crushed Lead
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_lead_pile'
-			}
-		],
-		result: {
-			item: 'create:crushed_raw_lead',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_lead_pile' )	
+    // Helper function for shapeless recipes
+    const createShapelessRecipe = (pile, raw) => {
+        event.custom({
+            type: "minecraft:crafting_shapeless",
+            ingredients: [{ item: `forge_frontier:${pile}` }],
+            result: { item: raw, count: 9 }
+        }).id(`forge_frontier:shapeless/${pile}`);
+    };
 
-	// Aluminum Pile to Crushed Lead
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_aluminum_pile'
-			}
-		],
-		result: {
-			item: 'create:crushed_raw_aluminum',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_aluminum_pile' )	
+    // Helper function for shaped recipes
+    const createShapedRecipe = (pile, raw) => {
+        event.shaped(
+            Item.of(`forge_frontier:${pile}`),
+            ['AAA', 'AAA', 'AAA'],
+            { A: raw }
+        ).id(`forge_frontier:shaped/${pile}`);
+    };
 
-	// Bismuth Pile to Crushed Bismuth
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_bismuth_pile'
-			}
-		],
-		result: {
-			item: 'forge_frontier:crushed_bismuth',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_bismuth_pile' )
+    // Create recipes for all ores
+    ores.forEach(ore => {
+        createShapelessRecipe(ore.pile, ore.raw);
+        createShapedRecipe(ore.pile, ore.raw);
+    });
 
-    // Desh Pile to Crushed Desh
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_desh_pile'
-			}
-		],
-		result: {
-			item: 'forge_frontier:crushed_desh',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_desh_pile' )
-
-    // Irradium Pile to Crushed Irradium
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_irradium_pile'
-			}
-		],
-		result: {
-			item: 'forge_frontier:crushed_irradium',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_irradium_pile' )
-
-    // Malachite Pile to Crushed Malachite
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_malachite_pile'
-			}
-		],
-		result: {
-			item: 'forge_frontier:crushed_malachite',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_malachite_pile' )
-
-    // Ostrum Pile to Crushed Ostrum
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_ostrum_pile'
-			}
-		],
-		result: {
-			item: 'forge_frontier:crushed_ostrum',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_ostrum_pile' )
-
-    // Tin Pile to Crushed Tin
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_tin_pile'
-			}
-		],
-		result: {
-			item: 'create:crushed_raw_tin',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_tin_pile' )
-
-    // Uranium Pile to Crushed Uranium
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_uranium_pile'
-			}
-		],
-		result: {
-			item: 'create:crushed_raw_uranium',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_uranium_pile' )
-
-    // Calorite Pile to Crushed Calorite
-	event.custom({
-		type: "minecraft:crafting_shapeless",
-		ingredients: [
-			{
-				item: 'forge_frontier:crushed_calorite_pile'
-			}
-		],
-		result: {
-			item: 'forge_frontier:crushed_calorite',
-			count: 9
-		  }
-	}).id( 'forge_frontier:shapeless/crushed_calorite_pile' )
-
-// Shaped Recipes
-
-	// Crushed Lead Pile Recipe
-    event.shaped(
-		Item.of('forge_frontier:crushed_lead_pile'),
-		[
-			'AAA',
-			'AAA',
-			'AAA'
-		],
-		{
-			A: 'create:crushed_raw_lead'
-		}
-	).id( 'forge_frontier:shaped/crushed_lead_pile' )	
-
-	// Crushed Aluminum Pile Recipe
-    event.shaped(
-		Item.of('forge_frontier:crushed_aluminum_pile'),
-		[
-			'AAA',
-			'AAA',
-			'AAA'
-		],
-		{
-			A: 'create:crushed_raw_aluminum'
-		}
-	).id( 'forge_frontier:shaped/crushed_aluminum_pile' )		
-
-    // Crushed Bismuth Pile Recipe
-    event.shaped(
-		Item.of('forge_frontier:crushed_bismuth_pile'),
-		[
-			'AAA',
-			'AAA',
-			'AAA'
-		],
-		{
-			A: 'forge_frontier:crushed_bismuth'
-		}
-	).id( 'forge_frontier:shaped/crushed_bismuth_pile' )
-
-    // Crushed Calorite Pile Recipe
-    event.shaped(
-		Item.of('forge_frontier:crushed_calorite_pile'),
-		[
-			'AAA',
-			'AAA',
-			'AAA'
-		],
-		{
-			A: 'forge_frontier:crushed_calorite'
-		}
-	).id( 'forge_frontier:shaped/crushed_calorite_pile' )
-
-    // Crushed Desh Pile Recipe
-    event.shaped(
-		Item.of('forge_frontier:crushed_desh_pile'),
-		[
-			'AAA',
-			'AAA',
-			'AAA'
-		],
-		{
-			A: 'forge_frontier:crushed_desh'
-		}
-	).id( 'forge_frontier:shaped/crushed_desh_pile' )
-
-    // Crushed Irradium Pile Recipe
-    event.shaped(
-		Item.of('forge_frontier:crushed_irradium_pile'),
-		[
-			'AAA',
-			'AAA',
-			'AAA'
-		],
-		{
-			A: 'forge_frontier:crushed_irradium'
-		}
-	).id( 'forge_frontier:shaped/crushed_irradium_pile' )
-
-    // Crushed Malachite Pile Recipe
-    event.shaped(
-        Item.of('forge_frontier:crushed_malachite_pile'),
-        [
-            'AAA',
-            'AAA',
-            'AAA'
-        ],
-        {
-            A: 'forge_frontier:crushed_malachite'
-        }
-    ).id( 'forge_frontier:shaped/crushed_malachite_pile' )
-
-    // Crushed Ostrum Pile Recipe
-    event.shaped(
-        Item.of('forge_frontier:crushed_ostrum_pile'),
-        [
-            'AAA',
-            'AAA',
-            'AAA'
-        ],
-        {
-            A: 'forge_frontier:crushed_ostrum'
-        }
-    ).id( 'forge_frontier:shaped/crushed_ostrum_pile' )
-
-    // Crushed Tin Pile Recipe
-    event.shaped(
-        Item.of('forge_frontier:crushed_tin_pile'),
-        [
-            'AAA',
-            'AAA',
-            'AAA'
-        ],
-        {
-            A: 'create:crushed_raw_tin'
-        }
-    ).id( 'forge_frontier:shaped/crushed_tin_pile' )
-
-    // Crushed Tin Pile Recipe
-    event.shaped(
-        Item.of('forge_frontier:crushed_uranium_pile'),
-        [
-            'AAA',
-            'AAA',
-            'AAA'
-        ],
-        {
-            A: 'create:crushed_raw_uranium'
-        }
-    ).id( 'forge_frontier:shaped/crushed_uranium_pile' )
-})
+});
