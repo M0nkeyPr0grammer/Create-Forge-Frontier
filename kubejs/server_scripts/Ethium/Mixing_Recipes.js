66d030b1b9d399e24090a69ddafd13712cc25749
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-01-28 15:01:25 -0600

    Fix Amethyst Mix Recipe

--- kubejs/server_scripts/Ethium/Mixing_Recipes.js
+++ kubejs/server_scripts/Ethium/Mixing_Recipes.js
@@ -1,28 +1,91 @@
 ServerEvents.recipes(event => {
 
-    // New Amethyst Mix Mixing Recipe
+    // New Amethyst Mix Mixing Recipe - 1 Thick Potion
     event.custom({
-        type: "create:mixing",
-        ingredients: [
-          {
-            item: "minecraft:amethyst_shard"
-          },
-          {
-            fluid: "create:potion",
-            nbt: {
-              Potion: "minecraft:thick"
-            },
-            amount: 1000
-          },
-        ],
-        results: [
-          {
-            count: 4,
-            item: "create_ethium:amethyst_mix",
+      type: "create:mixing",
+      ingredients: [
+        {
+          item: "minecraft:amethyst_shard"
+        },
+        {
+          item: "minecraft:potion",
+          nbt: {
+            Potion: "minecraft:thick"
           }
-        ],  
-        heatRequirement: "heated"
-      }).id('forge_frontier:mixing/amethyst_mix')
+        }
+      ],
+      results: [
+        {
+          item: "create_ethium:amethyst_mix",
+          count: 1
+        }
+      ],
+      heatRequirement: "heated"
+    }).id('forge_frontier:mixing/amethyst_mix_1');
+
+    // New Amethyst Mix Mixing Recipe - 2 Thick Potions
+    event.custom({
+      type: "create:mixing",
+      ingredients: [
+        {
+          item: "minecraft:amethyst_shard"
+        },
+        {
+          item: "minecraft:potion",
+          nbt: {
+            Potion: "minecraft:thick"
+          }
+        },
+        {
+          item: "minecraft:potion",
+          nbt: {
+            Potion: "minecraft:thick"
+          }
+        }
+      ],
+      results: [
+        {
+          item: "create_ethium:amethyst_mix",
+          count: 2
+        }
+      ],
+      heatRequirement: "heated"
+    }).id('forge_frontier:mixing/amethyst_mix_2');
+
+    // New Amethyst Mix Mixing Recipe - 3 Thick Potions
+    event.custom({
+      type: "create:mixing",
+      ingredients: [
+        {
+          item: "minecraft:amethyst_shard"
+        },
+        {
+          item: "minecraft:potion",
+          nbt: {
+            Potion: "minecraft:thick"
+          }
+        },
+        {
+          item: "minecraft:potion",
+          nbt: {
+            Potion: "minecraft:thick"
+          }
+        },
+        {
+          item: "minecraft:potion",
+          nbt: {
+            Potion: "minecraft:thick"
+          }
+        }
+      ],
+      results: [
+        {
+          item: "create_ethium:amethyst_mix",
+          count: 3
+        }
+      ],
+      heatRequirement: "heated"
+    }).id('forge_frontier:mixing/amethyst_mix_3');
 
     // New Echo Compound Mixing Recipe
     event.custom({
